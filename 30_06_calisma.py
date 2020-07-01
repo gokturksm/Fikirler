9dbab389ae0a1b3036149779f156606c7515eb58
gokturksm <gokturksm@gmail.com>
2020-07-02 00:10:13 +0300

    ana çalışmam için ilk deneme yapıldı.

--- 30_06_calisma.py
+++ 30_06_calisma.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import numpy as np
+
+value_list = ['fromP2P', 'toP2P', 'fromGRID', 'toGRID']
 
 df_pv = pd.Series([50, 50, 50, 50], name="time")
 df_loads = pd.DataFrame([[0, 0, 0, 0],
@@ -7,11 +8,43 @@ df_loads = pd.DataFrame([[0, 0, 0, 0],
                          [0, 10, 60, 55],
                          [99, 99, 99, 99]], columns=['h1', 'h2', 'h3', 'h4'])
 
-pv_ = df_pv.iloc[2]
-loads_ = df_loads.iloc[2]
+
+def make_df_header(loads_list, values_list):
+    return pd.DataFrame([], columns=pd.MultiIndex.from_product([values_list, loads_list.columns]))
+
+
+def make_equal(series: pd.Series, matched: float) -> pd.Series:
+    """
+    Equally distrubute a series considering a matched value.
+    Lower values than matched value are filtered.
+    :param series: A series which has one or more rows.
+    :param matched: A positive float number.
+    :return: A series that is equally distrubuted.
+    """
+    check_negative = series.sum() < 0
+    if check_negative:
+        sorted_ = series.abs().sort_values()
+    else:
+        sorted_ = series.sort_values()
+    per_ = matched / series.size
+    for i, v in enumerate(sorted_):
+        if not v > per_:
+            per_ = (matched - sorted_.iloc[:i + 1].sum()) / (series.size - (i + 1))
+        else:
+            break
+    sorted_.iloc[i:] = per_
+    if check_negative:
+        return series.mul(0).add(sorted_).mul(-1)
+    else:
+        return series.mul(0).add(sorted_)
+
 
 # geçici kwar değerlerini silmeyi unutma, gölgeleyenleri!
-def create_good_df(pv_: float = pv_, loads_: pd.Series = loads_):
+def create_good_df(pv_: float, loads_: pd.Series) -> pd.Series:
+    """
+    Creates one row of PV usage data.
+    """
+    time_ = loads_.name
     after_pv = loads_.sub(pv_ / loads_.size)
     buyers = after_pv.loc[after_pv >= 0.0]
     sellers = after_pv.loc[after_pv < 0.0]
@@ -19,36 +52,27 @@ def create_good_df(pv_: float = pv_, loads_: pd.Series = loads_):
     non_p2p_ = after_pv.sum()
     if p2p_ > 0:  # if p2p is happening before grid
         if non_p2p_ < 0:  # community is net seller
-            p2p_sellers = sellers.div(sellers.sum()).mul(p2p_)
+            p2p_sellers = make_equal(sellers, p2p_)
             p2p_buyers = buyers
         elif non_p2p_ > 0:  # community is net buyer
             p2p_sellers = sellers
-            p2p_buyers = buyers.div(buyers.sum()).mul(p2p_)
+            p2p_buyers = make_equal(buyers, p2p_)
         else:  # community is neuteral
             p2p_sellers, p2p_buyers = sellers, buyers
+    else:  # no p2p is happened, so now we head for the grid
+        p2p_sellers, p2p_buyers = sellers.mul(0.0), buyers.mul(0.0)
+    grid_sellers = sellers - p2p_sellers
+    grid_buyers = buyers - p2p_buyers
+    p2p_buyers.name, p2p_sellers.name = 'fromP2P', 'toP2P'
+    grid_buyers.name, grid_sellers.name = 'fromGRID', 'toGRID'
+    # make them all same sized
+    row_series = pd.DataFrame(data=[p2p_buyers, p2p_sellers, grid_buyers, grid_sellers]
+                              ).sort_index(axis=1).T.unstack()
+    row_series.name = time_
+    return row_series
 
-        def make_equal(series: pd.Series, matched: float) -> pd.Series:
-            """
-            Equally distrubute a series considering a matched value.
-            Lower values than matched value are filtered.
-            :param series: A series which has one or more rows.
-            :param matched: A positive float number.
-            :return: A series that is equally distrubuted.
-            """
-            check_negative = series.sum() < 0
-            if check_negative:
-                sorted_ = series.abs().sort_values()
-            else:
-                sorted_ = series.sort_values()
-            per_ = matched / series.size
-            for i, v in enumerate(sorted_):
-                if not v > per_:
-                    per_ = (matched - sorted_.iloc[:i+1].sum()) / (series.size - (i+1))
-                else:
-                    break
-            sorted_.iloc[i:] = per_
-            if check_negative:
-                return series.mul(0).add(sorted_).mul(-1)
-            else:
-                return series.mul(0).add(sorted_)
+
+df__ = make_df_header(df_loads, value_list)
+for ii in range(df_pv.size):
+    df__ = df__.append(create_good_df(df_pv.iloc[ii], df_loads.iloc[ii]))
 
--- so_7.py
+++ so_7.py
@@ -1,27 +1,6 @@
-import glob
-import csv
 import pandas as pd
 
+df = pd.DataFrame([[1]*5])
 
-def tabbed_to_df(file_name: str):
-    reader_output = []
-    with open(file_name, newline='') as csvfile:
-        f_reader = csv.reader(csvfile, delimiter='\t')
-        for row in f_reader:
-            reader_output.append(row)
-    df = pd.DataFrame(data=reader_output,
-                      columns=['Date', 'Price', 'Change', 'Change Percent', 'Change Cumulative Percent'],
-                      dtype='float')
-    df.set_index(['Date'], inplace=True)
-    df.index = pd.to_datetime(df.index, format="%d/%m/%Y")
-    return df
+print(df)
 
-
-file_names = ['GAU', 'IPP']
-file_paths = [f"{f}.txt" for f in file_names]
-isbank_data = [tabbed_to_df(f) for f in file_paths]
-
-all_data = pd.concat(isbank_data, keys=file_names)
-
-# all_data.loc[(slice(None), slice('2020-01', '2020-01')), :]
-# all_data.loc[('IPP', slice(None)), 'Change']
